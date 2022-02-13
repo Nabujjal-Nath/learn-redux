1c52c633006f348d63cce04434611ea00709eac6
nabujjal <nabujjal@gmail.com>
2022-02-13 23:14:56 +0530

    action&reducers

---
+++ src/reducers/incDec.js
@@ -0,0 +1,9 @@
+const incrementDecrement = (state = 0, action) => {
+    switch (action.type) {
+        case "INCREMENT": return state + 1;
+        case "DECREMENT": return state - 1;
+        default: return state;
+
+    }
+}
+export default incrementDecrement;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import incDecReducer from "./incDec"
+
+import { combineReducers } from "redux"
+const rootReducer = combineReducers({
+    incDecReducer
+})
+export default rootReducer;
